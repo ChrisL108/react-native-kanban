658f7b408586421a2ef694d4d0d9443384da0817
ChrisL108 <chrislacaille5@gmail.com>
2022-02-17 13:14:48 -0600

    add example w/ state; bump patch version

--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Button } from "react-native";
 
 export default function Kanban() {
+  const [count, setCount] = React.useState(0);
   return (
     <View>
-      <Text>Kanban coming soon!</Text>
+      <Button
+        title={`Kanban Coming Soon! (Press me?) ${count > 0 ? count : ""}`}
+        color="#841584"
+        onPress={() => setCount((prev) => prev + 1)}
+      />
     </View>
   );
 }
